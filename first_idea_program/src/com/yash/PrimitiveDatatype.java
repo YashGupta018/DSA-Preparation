19fd09dcb765b7d13aab9ef1c05ae45f7263f75b
YashGupta018 <ygupta2001@gmail.com>
2022-09-23 21:49:46 +0530

    Peimitive Datatypes

---
+++ first_idea_program/src/com/yash/PrimitiveDatatype.java
@@ -0,0 +1,19 @@
+package com.yash;
+
+public class PrimitiveDatatype {
+    public static void main(String[] args) {
+        //String is not primitive
+        int rollno = 12;    //rollno. Not a Wrapper Class
+        char letter = 'f';
+        float marks = 89.56f;
+        double largeDecimalNumber = 3452342343.525424;
+        long largeInteger = 2353453534534545L;
+        boolean check = true;
+
+        //Creating a class of integer
+
+        Integer rno = 34;   //Wrapper Classes
+        rno.byteValue();    //Wrapper Classes
+
+    }
+}
