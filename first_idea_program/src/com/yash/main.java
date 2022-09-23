40b64bbc8ebfa82971baf55c346e72cc0d284183
YashGupta018 <ygupta2001@gmail.com>
2022-09-23 21:47:52 +0530

    Initial Commit

---
+++ first_idea_program/src/com/yash/main.java
@@ -0,0 +1,12 @@
+package com.yash;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("hello world");
+        System.out.println(input.nextInt());
+    }
+}
